abc9eea8cfafcebec8484e07a102007a0f5e3f56
JULIA LIMA AMURIM <26171659@edu119.local>
2026-09-17 09:50:45 -0300

    atualizando cod

---
+++ servidor.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+let ALUNOS = [
+    {id: 1, nome: "Bernardo", curso: "Desenvolvimento de sistemas"},
+    {id: 2, nome: "Camily", curso: "Redes de computadores"},
+    {id: 3, nome: "Kaue", curso: "Banco de dados"},
+    {id: 4, nome: "Maria", curso: "Administração"},
+    {id: 5, nome: "Marjory", curso: "Desenvolvimento de sistemas"},
+];
+
+app.get("/", (req, res) => {
+    res.json({
+        mensagem: "API alunos funcionando"
+    })
+})
+
+const PORTA = 3000;
+app.listen(PORTA, () => {
+    console.log("Servidor iniciado com sucesso");
+    console.log(`http://localhost:${PORTA}`);
+})
