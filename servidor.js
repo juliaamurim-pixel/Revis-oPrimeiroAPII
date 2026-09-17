c7c3c2b825874f9b3c5083fcb9177ca1eeff89fb
JULIA LIMA AMURIM <26171659@edu119.local>
2026-09-17 14:16:46 -0300

    Subindo correção do projeto

--- servidor.js
+++ servidor.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 let ALUNOS = [
     {id: 1, nome: "Bernardo", curso: "Desenvolvimento de sistemas"},
@@ -10,18 +11,79 @@ let ALUNOS = [
     {id: 5, nome: "Marjory", curso: "Desenvolvimento de sistemas"},
 ];
 
-app.get("/", (req, res) => {
+app.get("/",(req,res) =>{
     res.json({
-        mensagem: "API alunos funcionando"
+        mensagem: "API Alunos funcionando"
     })
 });
 
-app.get("/alunos", (req, res) =>{
+app.get("/alunos",(req, res) =>{
     res.json(ALUNOS);
 });
 
+app.post("/alunos/cadastrar", (req, res)=>{
+    // console.log(req.body);
+    const {nome, curso} = req.body
+    // console.log("Nome:" + nome);
+    // console.log(`Curso: ${curso}`);
+
+    if(!nome || !curso){
+        return res.status(400).json({msg: "Nome e curso são obrigatorio"})
+    }
+
+    const id = ALUNOS.length > 0 ? ALUNOS[ALUNOS.length - 1].id + 1 : 1;                                     
+                                                         
+    const novoAluno = {            
+        id : id,
+        nome : nome,
+        curso : curso
+    };
+
+    ALUNOS.push(novoAluno);
+
+    res.status(201).json({msg: "Aluno cadastrado com sucesso."})
+
+});
+
+app.get("/alunos/:valor", (req,res) =>{
+    console.log(req)
+    const valor = Number(req.params.valor);
+
+    const aluno = ALUNOS.find(aluno => aluno.id === valor);
+    
+    if(!aluno){
+        return res.status(404).json({msg: "Aluno não encontrado"});
+    }
+    res.status(200).json(aluno);
+});
+
+app.put("/alunos/:valor",(req, res)=>{
+    const valor = Number(req.params.valor);
+    const {nome, curso} = req.body;
+
+    if(!nome || !curso){
+        return res.status(400).json({msg: "Nome e curso são obrigatorio"})
+    }
+
+    const indice = ALUNOS.findIndex(aluno => aluno.id === valor);
+    // console.log(indice)
+    if(indice === -1){
+        return res.status(404).json({msg: "Aluno não encontrado"})
+    }
+
+    ALUNOS[indice] ={
+        id: valor,
+        nome : nome,
+        curso: curso
+    };
+    
+    res.status(200).json({msg: "Aluno atualizado com sucesso"});
+
+})
+
+
 const PORTA = 3000;
-app.listen(PORTA, () => {
+app.listen(PORTA, ()=>{
     console.log("Servidor iniciado com sucesso");
     console.log(`http://localhost:${PORTA}`);
 })
